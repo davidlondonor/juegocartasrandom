723b5fcbea593f467d113e25a84884abc3566991
davidlucky@gmail.com <davidlucky@gmail.com>
2020-05-09 17:02:25 -0500

    ultimos cambios

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Juego from "./components/Juego";
 import Instrucciones from "./components/instrucciones/Instruccioness"
 import Inicio from "./components/inicio/inicio"
 import SaladeJuego from "./components/salaDeJuego/SalaDeJuego"
 
-function App() {
-    return (
 
-        <
-        div className="bigBox" >
-            <
-                Inicio />
-            <
-                Instrucciones />
-            <
-                SalaDeJuego />
-            <
-        /div>
+    class App extends React.Component {
+    constructor(props) {
+        super(props)
+    this.state = {}
 
-
-    );
+    
+        return ( <
+        div className = "bigBox" >
+            <
+        Inicio / >
+            <
+        Instrucciones / >
+            <
+        SalaDeJuego / >
+            <
+            /div>
+        );
+    }
 }
 
 export default App;
